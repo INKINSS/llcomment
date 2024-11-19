f1e736c764e0fa4c7f51f5030d13819a52135fdc
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-11-19 10:23:45 -0600

    Remove favicon.svg and add global CSS styles with Google Fonts; update Tailwind CSS configuration to extend colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const { nextui } = require("@nextui-org/react");
+const { nextui, colors } = require("@nextui-org/react");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",],
 	theme: {
 		extend: {
+			colors: {
+				primary: 'rgb(14,39,47)'
+			}
 		},
 	},
 	darkMode: 'class',
