1e6cbde96847363193d768047b7234ebe56332bb
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-11-27 09:01:28 -0600

    Integrate Toast UI editor for content input and enhance form styling

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,19 +1,28 @@
-import { Input } from "@nextui-org/input";
-import { Divider } from "@nextui-org/divider";
-import { Textarea } from "@nextui-org/react";
 import { useForm } from "react-hook-form";
 import { submitForm } from "../../utils/submitForm";
+import { Editor } from "@toast-ui/react-editor";
+import { useEffect, useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+import './styles.css'
 
 const Form = () => {
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
     reset,
+    setValue,
   } = useForm();
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    register("content", { required: true });
+  }, [register]);
 
   const onSubmit = async (data) => {
-    const result = await submitForm(data);
+    const content = editorRef.current.getInstance().getMarkdown();
+    setValue("content", content);
+    const result = await submitForm({...data, content});
     if (result) {
       reset();
     }
@@ -24,77 +33,29 @@ const Form = () => {
       onSubmit={handleSubmit(onSubmit)}
       className="flex flex-col ssm:w-full md:w-[70%] ssm:px-9 md:px-12 py-8 rounded-lg prose lg:prose-xl md:prose-xl"
     >
-      <Input
-        type="text"
-        label="nickname"
-        placeholder="ej:@samito"
-        isRequired
-        variant="bordered"
-        size="lg"
-        isInvalid={errors.nickname?.type === "required" && true}
-        errorMessage={
-          errors.nickname?.type === "required" &&
-          "registra un nick para tu artículo"
-        }
-        {...register("nickname", {
-          required: true,
-        })}
-      />
-      <Divider className="my-5 md:my-5" />
-      <Input
-        type="text"
-        label="título de tu artículo"
-        placeholder="ej:herramientas para usar con react"
-        isRequired
-        variant="bordered"
-        size="lg"
-        isInvalid={errors.title?.type === "required" && true}
-        errorMessage={
-          errors.title?.type === "required" &&
-          "es necesario un titulo principal"
-        }
-        {...register("title", {
-          required: true,
-        })}
-      />
-      <Divider className="my-5 md:my-5" />
-      <Input
-        type="array"
-        label="etiquetas"
-        placeholder="ej: javascript, librerías, node, tailwind"
-        isRequired
-        variant="bordered"
-        size="lg"
-        isInvalid={errors.tags?.type === "required" && true}
-        errorMessage={
-          errors.tags?.type === "required" &&
-          "es necesario al menos una etiqueta"
-        }
-        {...register("tags", {
-          required: true,
-        })}
-      />
-      <h2 className="text-primary ssm:text-[1.3rem] md:text-[1.6rem] ssm:mt-6 font-semibold">
-        contenido principal
-      </h2>
-      <Textarea
+      <Editor
+        ref={editorRef}
+        previewStyle="tab"
+        height="300px"
+        initialEditType="markdown"
         placeholder="escribe aquí tu artículo"
-        variant="bordered"
-        size="lg"
-        isRequired
-        rows={10}
-        isInvalid={errors.content?.type === "required" && true}
-        errorMessage={
-          errors.content?.type === "required" && "es necesario un contenido"
-        }
-        {...register("content", {
-          required: true,
-        })}
+        useCommandShortcut={true}
+        toolbarItems={[
+          ["heading", "bold", "italic", "strike"],
+          ["hr", "quote"],
+          ["ul", "ol", "task"],
+          ["table", "link"],
+          ["code", "codeblock"],
+        ]}
+        onChange={() => {
+          const content = editorRef.current?.getInstance().getMarkdown();
+          setValue("content", content, { shouldValidate: true });
+        }}
       />
       <input
         type="submit"
         value={"enviar"}
-        className="bg-primary text-white py-2 my-5 rounded-lg text-center cursor-pointer hover:bg-purplePrimary transition duration-200"
+        className="bg-primary text-white py-2 mt-10 mb-5 rounded-lg text-center cursor-pointer hover:bg-purplePrimary transition duration-200"
       />
     </form>
   );
