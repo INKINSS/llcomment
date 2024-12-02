2a419f5495d80f18f17f4a90539c834cb801e629
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-12-02 09:31:33 -0600

    Add BackDiscover and BackHome components for navigation

---
+++ src/components/common/buttons/BackDiscover.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const BackDiscover = () => {
+  return (
+    <div className="flex justify-center mb-10">
+    <a href='/discover'
+      className="inline-block md:text-[1.2rem] underline font-bold text-center prose md:prose-xl"
+    >
+      regresar
+    </a>
+  </div>
+  )
+}
---
+++ src/components/common/buttons/BackHome.tsx
@@ -0,0 +1,12 @@
+
+export const BackHome = () => {
+  return (
+    <div className="flex justify-center my-10">
+    <a href='/'
+      className="inline-block md:text-[1.2rem] underline font-bold text-center prose md:prose-xl"
+    >
+      inicio
+    </a>
+  </div>
+  )
+}
