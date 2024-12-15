3a7950c67d432a320f254009bb8e5554ac4d195e
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-12-14 18:16:33 -0600

    Add Vercel adapter and update static paths fallback in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless'
 
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({}),
   devToolbar: {enabled: false},
   integrations: [tailwind(), react()],
   server: {
     host: true,
     port: 4321,
-  }
+  },
 }
 );
