941b0358671ad0c52ab9ac0835adc98b89591dd7
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-12-19 13:51:20 -0600

    Update Vercel adapter configuration and add .vercel to .gitignore

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel'
+import vercel from '@astrojs/vercel/serverless'
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  }),
   devToolbar: {enabled: false},
   integrations: [tailwind(), react()],
   server: {
