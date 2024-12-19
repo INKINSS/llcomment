d32a2c4df3ee5f1829454ae82710aac380f2d371
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-12-19 11:57:43 -0600

    update vercel adapter configuration and change output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel'
 
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({}),
+  output: 'server',
+  adapter: vercel(),
   devToolbar: {enabled: false},
   integrations: [tailwind(), react()],
   server: {
