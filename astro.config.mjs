5f1ab63ed79a9dd8d485f145fc9d0db0f3050a77
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-12-19 15:09:16 -0600

    Update Vercel adapter to use serverless configuration and change output to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless'
+import vercelServerless from '@astrojs/vercel/serverless'
 
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    }
-  }),
+  output: 'hybrid',
+  adapter: vercelServerless({}),
   devToolbar: {enabled: false},
   integrations: [tailwind(), react()],
   server: {
