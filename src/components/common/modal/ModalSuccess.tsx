ca0f140c9238f1ba1fbbb7778badb04a7e810741
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2024-12-21 19:43:09 -0600

    remove backdrop and refactory with onClose and isOpen

--- src/components/common/modal/ModalSuccess.tsx
+++ src/components/common/modal/ModalSuccess.tsx
@@ -10,24 +10,20 @@ import {
 
 const ModalSuccess = ({ visible, onClose, onViewPost, postSlug }) => {
   return (
-    <Modal backdrop='opaque' isOpen={visible} onOpenChange={onClose}>
+    <Modal isOpen={visible} onClose={onClose}>
       <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1">Publicación Exitosa</ModalHeader>
-            <ModalBody>
-              <p>¡Felicidades! Tu artículo ha sido publicado.</p>
-            </ModalBody>
-            <ModalFooter>
-              <Button onClick={() => onViewPost(postSlug)} color="primary">
-                Ver Publicación
-              </Button>
-              <Button color="secondary" onClick={onClose}>
-                Cerrar
-              </Button>
-            </ModalFooter>
-          </>
-        )}
+        <ModalHeader className="flex flex-col gap-1">Publicación Exitosa</ModalHeader>
+        <ModalBody>
+          <p>¡Felicidades! Tu artículo ha sido publicado.</p>
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={() => onViewPost(postSlug)} color="primary">
+            Ver Publicación
+          </Button>
+          <Button color="secondary" onClick={onClose}>
+            Cerrar
+          </Button>
+        </ModalFooter>
       </ModalContent>
     </Modal>
   );
