fe22c82da2b20ddd99e8ec26d94b39823c85402a
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-01-10 16:00:25 -0600

    refactoring slug and fetchData

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -4,17 +4,20 @@ import { database } from "../../firebaseConfig";
 export const fetchData = async (path: string) => {
   const dbRef = ref(database);
   try {
+    console.log(`Fetching data from path: ${path}`);
     const snapshot = await get(child(dbRef, path));
+    
     if (snapshot.exists()) {
       const data = snapshot.val();
-      // Accede a la clave "posts" y asegúrate de devolver siempre un array
-      return data || [];
+      console.log(`Data found for path ${path}:`, data);
+      // Si data es null o undefined, retorna un array vacío
+      return Array.isArray(data) ? data : Object.values(data);
     } else {
-      console.log("No data available");
+      console.warn(`No data available for path: ${path}`);
       return [];
     }
   } catch (error) {
-    console.error("Error fetching data:", error);
-    return [];
+    console.error(`Error fetching data from path ${path}:`, error);
+    throw new Error(`Failed to fetch data: ${error.message}`);
   }
 };
