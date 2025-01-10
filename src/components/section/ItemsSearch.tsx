6b45f327dba1e14a1f663026e41f1a3103da9fad
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-01-10 09:28:01 -0600

    Refactor ItemsSearch and [slug].astro components to utilize generateSlug from utils, improving slug generation consistency and code clarity

--- src/components/section/ItemsSearch.tsx
+++ src/components/section/ItemsSearch.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { fetchData } from '../../utils/fetchData';
-import InputSearch from '../common/Inputs/InputSearch';
-import SkeletonPage from '../skeleton/Skeleton';
+import React, { useEffect, useState } from "react";
+import { fetchData } from "../../utils/fetchData";
+import InputSearch from "../common/Inputs/InputSearch";
+import SkeletonPage from "../skeleton/Skeleton";
+import { generateSlug } from "../../utils/generateSlug";
 
 interface Post {
   id: string;
@@ -14,31 +15,25 @@ interface Post {
 
 const ItemsSearch = () => {
   const [data, setData] = useState<Post[]>([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [loading, setLoading] = useState(true);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
   };
 
-  const generateSlug = (title: string) => {
-    return title
-      .toLowerCase()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .replace(/ /g, '-')
-      .replace(/[^\w-]+/g, '');
-  };
-
-  const filteredData = data.filter((item) =>
-    item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    item.tags.some((tag) => tag.toLowerCase().includes(searchTerm.toLowerCase()))
+  const filteredData = data.filter(
+    (item) =>
+      item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.tags.some((tag) =>
+        tag.toLowerCase().includes(searchTerm.toLowerCase())
+      )
   );
 
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
-      const result = await fetchData('posts');
+      const result = await fetchData("posts");
       if (result && result.length > 0) {
         setData(result);
       }
@@ -49,31 +44,36 @@ const ItemsSearch = () => {
   }, []);
 
   return (
-    <main className='ssm:w-full flex flex-col items-center'>
+    <main className="ssm:w-full flex flex-col items-center">
       <InputSearch onChange={handleSearchChange} />
-      <section className='flex flex-wrap ssm:w-full px-4 mt-10 justify-evenly rounded-lg ssm:flex-col md:flex-row ssm:items-center hover'>
+      <section className="flex flex-wrap ssm:w-full px-4 mt-10 justify-evenly rounded-lg ssm:flex-col md:flex-row ssm:items-center hover">
         {loading ? (
           <SkeletonPage count={data.length || 6} />
-        ) : 
-        filteredData.length > 0 ? (
+        ) : filteredData.length > 0 ? (
           filteredData.map((item) => (
             <a href={`/posts/${generateSlug(item.title)}`} key={item.id}>
-              <article className='ssm:w-[20rem] sm:w-[23rem] min-h-[13rem] py-4 px-8 my-4 shadow-custom rounded-md relative hover:scale-105 transform duration-200 cursor-pointer'>
-                <h3 className='ssm:text-[1.2rem] sm:text-[1.4rem] font-semibold line-clamp-1'>{item.title}</h3>
-                <p className='text-gray-900 line-clamp-2'>{item.content}</p>
-                <div className='my-3 flex flex-wrap'>
+              <article className="ssm:w-[20rem] sm:w-[23rem] min-h-[13rem] py-4 px-8 my-4 shadow-custom rounded-md relative hover:scale-105 transform duration-200 cursor-pointer">
+                <h3 className="ssm:text-[1.2rem] sm:text-[1.4rem] font-semibold line-clamp-1">
+                  {item.title}
+                </h3>
+                <p className="text-gray-900 line-clamp-2">{item.content}</p>
+                <div className="my-3 flex flex-wrap">
                   {item.tags.map((tag, index) => (
                     <span
                       key={index}
-                      className='ssm:text-[.8rem] ssm:my-1 sm:my-0 sm:text-[.9rem] ssm:px-2 ssm:py-1 sm:px-3 sm:py-1 bg-gray-200 text-gray-700 rounded-full mr-2'
+                      className="ssm:text-[.8rem] ssm:my-1 sm:my-0 sm:text-[.9rem] ssm:px-2 ssm:py-1 sm:px-3 sm:py-1 bg-gray-200 text-gray-700 rounded-full mr-2"
                     >
                       {tag}
                     </span>
                   ))}
                 </div>
-                <div className='flex justify-between'>
-                  <span className='inline-block mt-3 ssm:text-[.8rem] md:text-[.9rem] text-grayPrimary bottom-5'>{item.nickname}</span>
-                  <span className='inline-block mt-3 ssm:text-[.8rem] md:text-[.9rem] text-grayPrimary bottom-5'>{item.publishDate}</span>
+                <div className="flex justify-between">
+                  <span className="inline-block mt-3 ssm:text-[.8rem] md:text-[.9rem] text-grayPrimary bottom-5">
+                    {item.nickname}
+                  </span>
+                  <span className="inline-block mt-3 ssm:text-[.8rem] md:text-[.9rem] text-grayPrimary bottom-5">
+                    {item.publishDate}
+                  </span>
                 </div>
               </article>
             </a>
